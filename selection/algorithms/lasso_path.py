64544a389af7846f020672009f7d4cd78a7bf81e
Jonathan Taylor <jonathan.taylor@stanford.edu>
2015-10-31 23:43:32 -0700

    drop in SS sort of working -- needs a test

--- selection/algorithms/lasso_path.py
+++ selection/algorithms/lasso_path.py
@@ -15,13 +15,14 @@ from selection.constraints.affine import (constraints, gibbs_test,
 
 class lasso_path(lasso):
 
-    def __init__(self, X, y, lambda_values, feature_weights=None):
+    def __init__(self, X, y, lambda_values, feature_weights=None,
+                 sigma=1.):
         n, p = X.shape
         self.X = X
         self.y = y
         self.lambda_values = lambda_values
         self.ever_active = set([])
-
+        self.sigma = 1. # sigma
         self.loss = rr.squared_error(X, y)
 
         if feature_weights is None:
@@ -31,7 +32,7 @@ class lasso_path(lasso):
         self.problem = rr.simple_problem(self.loss, self.penalty)
         self.soln_path = []
 
-    def fit(self, test_statistic, burnin=5000, ndraw=20000,
+    def fit(self, test_statistic, burnin=10000, ndraw=30000,
             **solve_args):
         n, p = self.X.shape
 
@@ -79,14 +80,31 @@ class lasso_path(lasso):
                 conL = constraints(-self.X[:,always_inactive].T,
                                    -overall_lower_bd)
                 inactive_con = con_stack(conU, conL)
+                inactive_con.covariance = self.sigma**2 * np.identity(self.y.shape[0])
                 if ever_active:
                     conditional_con = inactive_con.conditional( \
                         self.X[:,ever_active].T,
                         np.dot(self.X[:,ever_active].T, self.y))
+                    P1 = np.dot(self.X[:,ever_active],
+                                np.linalg.pinv(self.X[:,ever_active]))
                 else:
                     conditional_con = inactive_con
+                    P1 = np.zeros((self.y.shape[0],)*2)
 
-                print conditional_con(self.y)
+                print np.diag(inactive_con.covariance).sum()
+                full_active = sorted(self.ever_active.union(diff))
+                P2 = np.dot(self.X[:,full_active],
+                            np.linalg.pinv(self.X[:,full_active]))
+                R = P1 - P2
+
+                def test_statisticR(Z, R=R):
+                    delta = np.dot(Z, R)
+                    print 'here'
+                    if delta.ndim == 1:
+                        return (delta**2).sum()
+                    else:
+                        print (delta**2).shape
+                        return (delta**2).sum(1)
 
                 pval = gibbs_test(conditional_con,
                                   self.y,
@@ -95,24 +113,25 @@ class lasso_path(lasso):
                                   white=False,
                                   ndraw=ndraw,
                                   burnin=burnin,
+                                  how_often=-1,
                                   UMPU=False,
                                   use_random_directions=False,
                                   tilt=None,
                                   alternative='greater',
-                                  test_statistic=test_statistic,
+                                  test_statistic=test_statisticR,
                                   )[0]
-                print pval, diff, self.ever_active
+                print pval, sorted(diff), sorted(self.ever_active)
                 self.ever_active = self.ever_active.union(active_set)
 
 
 def main():
 
     from selection.algorithms.lasso import instance
-    X, y = instance()[:2]
-    lam_values = np.linspace(0.05, 1, 20) * np.fabs(np.dot(X.T, y)).max()
+    X, y, _, _, sigma = instance()
+    lam_values = np.linspace(0.05, 1, 51) * np.fabs(np.dot(X.T, y)).max()
     lam_values = lam_values[::-1]
 
-    las_path = lasso_path(X, y, lam_values)
+    las_path = lasso_path(X, y, lam_values, sigma=sigma)
     las_path.fit(None, tol=1.e-12, min_its=100)
 
 if __name__ == "__main__":
