fcb686e1e05571883a5e29865cdc49c9b1f2a5cd
Snigdha Panigrahi <snigdhapanigrahi@DN0a22c446.SUNet>
2017-03-13 16:59:01 -0700

    added sh file

---
+++ selection/frequentist_eQTL/tests/submit_python_jobs.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+# Setup bash job headers
+
+# load local environment
+
+# setup dir if needed
+
+DIR=/scratch/users/snigdha/freq_cis_eqtl/outputs
+
+mkdir -p $DIR
+
+for i in {0..10}
+do
+	# bash single_python_run.sbatch $i $DIR
+	sbatch single_python_run.sbatch $i $DIR
+done
