8fd4764193235da95eb973e4547b27ba79e4a6a8
Snigdha Panigrahi <snigdhapanigrahi@DN51sl2p.SUNet>
2017-03-18 12:19:42 -0700

    changes in number of jobs

--- selection/frequentist_eQTL/tests/submit_python_jobs.sh
+++ selection/frequentist_eQTL/tests/submit_python_jobs.sh
@@ -9,7 +9,7 @@ DIR=/scratch/users/snigdha/freq_cis_eqtl/outputs
 
 mkdir -p $DIR
 
-for i in {0..10}
+for i in {0..50}
 do
 	# bash single_python_run.sbatch $i $DIR
 	sbatch single_python_run.sbatch $i $DIR
