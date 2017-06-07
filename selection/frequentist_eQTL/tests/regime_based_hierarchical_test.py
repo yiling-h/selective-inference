5f48d76087b1f9525d6075bb1c2b85ac2fc4a0d1
Snigdha Panigrahi <snigdhapanigrahi@DN51sla7.SUNet>
2017-06-06 21:32:22 -0700

    added functions

--- selection/frequentist_eQTL/tests/regime_based_hierarchical_test.py
+++ selection/frequentist_eQTL/tests/regime_based_hierarchical_test.py
@@ -4,8 +4,11 @@ import os
 from scipy.stats import norm as normal
 
 import numpy as np
-import regreg.api as rr
 
+import regreg.api as rr
+from selection.bayesian.selection_probability_rr import nonnegative_softmax_scaled
+
+from selection.frequentist_eQTL.approx_confidence_intervals import neg_log_cube_probability
 #from selection.frequentist_eQTL.estimator import M_estimator_2step
 from selection.randomized.M_estimator import M_estimator
 #from selection.frequentist_eQTL.approx_ci_2stage import approximate_conditional_prob_2stage, approximate_conditional_density_2stage
@@ -13,10 +16,8 @@ from selection.randomized.M_estimator import M_estimator
 from selection.randomized.query import naive_confidence_intervals
 from selection.randomized.query import naive_pvalues
 
-from selection.bayesian.initial_soln import instance
 #from selection.frequentist_eQTL.simes_BH_selection import BH_selection_egenes, simes_selection_egenes
 from selection.bayesian.cisEQTLS.Simes_selection import BH_q
-#from selection.frequentist_eQTL.instance import instance
 from selection.tests.instance import gaussian_instance
 
 class simes_selection_egenes():
