acf7c767f6b41a5e050564ab9309c4bda0882efa
Snigdha Panigrahi <snigdhapanigrahi@DN0a22c261.SUNet>
2017-06-15 16:00:55 -0700

    commit test for simes

---
+++ selection/frequentist_eQTL/tests/submit_simes_jobs.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+# Setup bash job headers
+
+# load local environment
+
+# setup dir if needed
+
+
+for i in $(seq -f %03g 1 100)
+do
+    IN=/scratch/PI/sabatti/controlled_access_data/temp_transfer/Muscle_Skeletal_mixture4amp0.30/Muscle_Skeletal_chunk${i}_mtx/
+    OUT=/scratch/PI/jtaylo/snigdha_data/gtex/simulation_muscle/Muscle_Skeletal_chunk${i}_mtx/
+    mkdir -p ${OUT}
+    echo "${IN}"
+    echo "${OUT}"
+	bash single_simes_python.sbatch ${IN} ${OUT}
+	#sbatch single_simes_python.sbatch ${IN} ${OUT}
+done
