52dd1338eeded95e96bf275946637bef75c151c7
Snigdha Panigrahi <snigdhapanigrahi@DN51slq4.SUNet>
2017-06-15 20:10:58 -0700

    changed output file

--- selection/frequentist_eQTL/tests/submit_simes_jobs.sh
+++ selection/frequentist_eQTL/tests/submit_simes_jobs.sh
@@ -9,10 +9,10 @@
 for i in $(seq -f %03g 1 100)
 do
     IN=/scratch/PI/sabatti/controlled_access_data/temp_transfer/Muscle_Skeletal_mixture4amp0.30/Muscle_Skeletal_chunk${i}_mtx/
-    OUT=/scratch/PI/jtaylo/snigdha_data/gtex/simulation_muscle/Muscle_Skeletal_chunk${i}_mtx/
+    OUT=/scratch/PI/jtaylo/snigdha_data/gtex/simulation_muscle/Muscle_Skeletal_chunk${i}_mtx/${i}_
     mkdir -p ${OUT}
-    echo "${IN}"
-    echo "${OUT}"
+    #echo "${IN}"
+    #echo "${OUT}"
 	# bash single_simes_python.sbatch ${IN} ${OUT}
 	sbatch single_simes_python.sbatch ${IN} ${OUT}
 done
