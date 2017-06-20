27f586c6f88022ff15d4dfd19f5e2f6b2ad1f6d5
Snigdha Panigrahi <snigdhapanigrahi@DN51slq4.SUNet>
2017-06-19 21:18:42 -0700

    changed output file

--- selection/frequentist_eQTL/tests/egene_selection_simes.py
+++ selection/frequentist_eQTL/tests/egene_selection_simes.py
@@ -58,7 +58,7 @@ if __name__ == "__main__":
     outdir = sys.argv[2]
     result = sys.argv[3]
 
-    outfile = os.path.join(outdir, "simes_output_rand_0.10_"+ str(result) + ".txt")
+    outfile = os.path.join(outdir, "simes_output_norand_"+ str(result) + ".txt")
 
     gene_file = path + "Genes.txt"
 
