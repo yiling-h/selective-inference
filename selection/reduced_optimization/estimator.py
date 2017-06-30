4c78379d5d7e7aebf242f4f753777347a7305e90
Snigdha Panigrahi <snigdhapanigrahi@DN51skqu.SUNet>
2017-06-29 19:38:36 -0700

    change

--- selection/reduced_optimization/estimator.py
+++ selection/reduced_optimization/estimator.py
@@ -2,6 +2,60 @@ import numpy as np
 from selection.randomized.M_estimator import M_estimator, M_estimator_split
 from selection.randomized.glm import pairs_bootstrap_glm, bootstrap_cov
 
+class M_estimator_exact(M_estimator):
+
+    def __init__(self, loss, epsilon, penalty, randomization, randomizer='gaussian'):
+        M_estimator.__init__(self, loss, epsilon, penalty, randomization)
+        self.randomizer = randomizer
+
+    def solve_approx(self):
+        np.random.seed(0)
+        self.solve()
+        (_opt_linear_term, _opt_affine_term) = self.opt_transform
+        self._opt_linear_term = np.concatenate(
+            (_opt_linear_term[self._overall, :], _opt_linear_term[~self._overall, :]), 0)
+        self._opt_affine_term = np.concatenate((_opt_affine_term[self._overall], _opt_affine_term[~self._overall]), 0)
+        self.opt_transform = (self._opt_linear_term, self._opt_affine_term)
+
+        (_score_linear_term, _) = self.score_transform
+        self._score_linear_term = np.concatenate(
+            (_score_linear_term[self._overall, :], _score_linear_term[~self._overall, :]), 0)
+        self.score_transform = (self._score_linear_term, np.zeros(self._score_linear_term.shape[0]))
+        self.feasible_point = np.abs(self.initial_soln[self._overall])
+        lagrange = []
+        for key, value in self.penalty.weights.iteritems():
+            lagrange.append(value)
+        lagrange = np.asarray(lagrange)
+        self.inactive_lagrange = lagrange[~self._overall]
+
+        X, _ = self.loss.data
+        n, p = X.shape
+        self.p = p
+
+        nactive = self._overall.sum()
+        score_cov = np.zeros((p, p))
+        X_active_inv = np.linalg.inv(X[:,self._overall].T.dot(X[:,self._overall]))
+        projection_perp = np.identity(n) - X[:,self._overall].dot(X_active_inv).dot( X[:,self._overall].T)
+        score_cov[:nactive, :nactive] = X_active_inv
+        score_cov[nactive:, nactive:] = X[:,~self._overall].T.dot(projection_perp).dot(X[:,~self._overall])
+
+        self.score_target_cov = score_cov[:, :nactive]
+        self.target_cov = score_cov[:nactive, :nactive]
+        self.target_observed = self.observed_score_state[:nactive]
+        self.nactive = nactive
+
+        self.B_active = self._opt_linear_term[:nactive, :nactive]
+        self.B_inactive = self._opt_linear_term[nactive:, :nactive]
+
+
+    def setup_map(self, j):
+
+        self.A = np.dot(self._score_linear_term, self.score_target_cov[:, j]) / self.target_cov[j, j]
+        self.null_statistic = self._score_linear_term.dot(self.observed_score_state) - self.A * self.target_observed[j]
+
+        self.offset_active = self._opt_affine_term[:self.nactive] + self.null_statistic[:self.nactive]
+        self.offset_inactive = self.null_statistic[self.nactive:]
+
 class M_estimator_approx(M_estimator):
 
     def __init__(self, loss, epsilon, penalty, randomization, randomizer, estimation):
--- selection/reduced_optimization/tests/egene.py
+++ selection/reduced_optimization/tests/egene.py
@@ -1,11 +1,13 @@
 import os, numpy as np, pandas, statsmodels.api as sm
 import time
+import sys
 
 import regreg.api as rr
 from selection.bayesian.initial_soln import selection
 from selection.randomized.api import randomization
 from selection.reduced_optimization.lasso_reduced import nonnegative_softmax_scaled, neg_log_cube_probability, selection_probability_lasso, \
     sel_prob_gradient_map_lasso, selective_inf_lasso
+from selection.reduced_optimization.estimator import M_estimator_exact
 
 def estimate_sigma(X, y, nstep=20, tol=1.e-4):
 
@@ -41,33 +43,40 @@ y = np.load(os.path.join(path + "y_" + "ENSG00000131697.13") + ".npy")
 y = y.reshape((y.shape[0],))
 
 sigma = estimate_sigma(X, y, nstep=20, tol=1.e-5)
-#sigma = 0.37858429815791306
 y /= sigma
 tau = 1.
 
+
+lam = 1. * np.mean(np.fabs(np.dot(X.T, np.random.standard_normal((n, 2000)))).max(0)) * 1.
+loss = rr.glm.gaussian(X, y)
+
+epsilon = 1. / np.sqrt(n)
+
+W = np.ones(p) * lam
+penalty = rr.group_lasso(np.arange(p),
+                         weights=dict(zip(np.arange(p), W)), lagrange=1.)
+
 np.random.seed(4)
-random_Z = np.random.normal(loc=0.0, scale= 1., size= p)
-#random_Z = np.zeros(p)
-sel = selection(X, y, random_Z, randomization_scale=tau)
+randomization = randomization.isotropic_gaussian((p,), scale=1.)
 
-lam, epsilon, active, betaE, cube, initial_soln = sel
+M_est = M_estimator_exact(loss, epsilon, penalty, randomization, randomizer='gaussian')
 
-print("value of tuning parameter",lam)
-print("nactive", active.sum())
-active_set = [i for i in range(p) if active[i]]
-print("active variables", active_set)
-print("initial lasso", betaE)
+M_est.solve_approx()
+active = M_est._overall
+active_set = np.asarray([i for i in range(p) if active[i]])
+nactive = np.sum(active)
+betaE = M_est.initial_soln[M_est._overall]
 
 noise_variance = 1.
-print("noise variance", noise_variance)
-nactive = betaE.shape[0]
 active_sign = np.sign(betaE)
 feasible_point = np.fabs(betaE)
 lagrange = lam * np.ones(p)
 
+sys.stderr.write("number of active selected by lasso" + str(nactive) + "\n")
+sys.stderr.write("Active set selected by lasso" + str(active_set) + "\n")
+
 generative_X = X[:, active]
 prior_variance = 1000.
-randomizer = randomization.isotropic_gaussian((p,), tau)
 
 Q = np.linalg.inv(prior_variance* (generative_X.dot(generative_X.T)) + noise_variance* np.identity(n))
 post_mean = prior_variance * ((generative_X.T.dot(Q)).dot(y))
@@ -78,7 +87,22 @@ print("unadjusted estimates", (sigma* unadjusted_intervals).T)
 cov = np.linalg.inv(X[:,active].T.dot(X[:, active])+ epsilon * np.identity(nactive))
 unadjusted_intervals[0,:] = cov.dot(X[:,active].T.dot(X[:,active])).dot(unadjusted_intervals[0,:])
 unadjusted_intervals[1,:] = cov.dot(X[:,active].T.dot(X[:,active])).dot(unadjusted_intervals[1,:])
-print("unadjusted estimates", (sigma* unadjusted_intervals).T)
+print("unadjusted estimates 1", (sigma* unadjusted_intervals).T)
+
+projection_active = X[:, active].dot(np.linalg.inv(X[:, active].T.dot(X[:, active])+ epsilon * np.identity(nactive)))
+M_1 = prior_variance * (X[:, active].dot(X[:, active].T)) + noise_variance * np.identity(n)
+M_2 = prior_variance * ((X[:, active].dot(X[:, active].T)).dot(projection_active))
+M_3 = prior_variance * (projection_active.T.dot(X[:, active].dot(X[:, active].T)).dot(projection_active))
+post_mean = M_2.T.dot(np.linalg.inv(M_1)).dot(y)
+
+post_var = M_3 - M_2.T.dot(np.linalg.inv(M_1)).dot(M_2)
+
+unadjusted_intervals = np.vstack([post_mean - 1.65 * (np.sqrt(post_var.diagonal())),
+                                  post_mean + 1.65 * (np.sqrt(post_var.diagonal()))])
+print("unadjusted estimates 2", (unadjusted_intervals).T)
+
+print("check",np.where(X[:,active].dot(np.linalg.inv(X[:, active].T.dot(X[:, active]))).
+               dot(X[:, active].T.dot(X[:, active])+ epsilon * np.identity(nactive))>10000))
 
 grad_map = sel_prob_gradient_map_lasso(X,
                                        feasible_point,
@@ -87,7 +111,7 @@ grad_map = sel_prob_gradient_map_lasso(X,
                                        lagrange,
                                        generative_X,
                                        noise_variance,
-                                       randomizer,
+                                       randomization,
                                        epsilon)
 
 inf = selective_inf_lasso(y, grad_map, prior_variance)
@@ -101,7 +125,12 @@ print('sampling time', tic - toc)
 
 
 adjusted_intervals = np.vstack([np.percentile(samples, 5, axis=0), np.percentile(samples, 95, axis=0)])
-sel_mean = np.mean(samples, axis=0)
+adjusted_intervals[0,:] = cov.dot(X[:,active].T.dot(X[:,active])).dot(adjusted_intervals[0,:])
+adjusted_intervals[1,:] = cov.dot(X[:,active].T.dot(X[:,active])).dot(adjusted_intervals[1,:])
+print("unadjusted estimates", (sigma* unadjusted_intervals).T)
+
+sel_mean = cov.dot(X[:,active].T.dot(X[:,active])).dot(np.mean(samples, axis=0))
+
 
 print("active variables", active_set)
 print("\n")
