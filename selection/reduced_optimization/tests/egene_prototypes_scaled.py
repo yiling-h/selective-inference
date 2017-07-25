c719e432089d753dac439f9136c1b08b4ab50f2f
Snigdha Panigrahi <snigdhapanigrahi@DN51sljk.SUNet>
2017-07-25 01:03:32 -0700

    changed lambda

--- selection/reduced_optimization/tests/egene_prototypes_scaled.py
+++ selection/reduced_optimization/tests/egene_prototypes_scaled.py
@@ -15,7 +15,7 @@ def selection(X, y, random_Z, randomization_scale=1., sigma=None, method="theore
     n, p = X.shape
     loss = rr.glm.gaussian(X,y)
     epsilon = 1. / np.sqrt(n)
-    lam_frac = 0.8
+    lam_frac = 1.
     if sigma is None:
         sigma = 1.
     if method == "theoretical":
