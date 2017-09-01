497b99a0802a139ee6ea8a9e40bbb649d14a5e12
Snigdha Panigrahi <snigdhapanigrahi@DN0a22c54f.SUNet>
2017-09-01 09:30:03 -0700

    added local processing files for sim Y

---
+++ selection/frequentist_eQTL/simulated_data_pipeline/sim_BH_simes.py
@@ -0,0 +1,94 @@
+import glob
+import os, numpy as np, pandas, statsmodels.api as sm
+
+def BH_selection_egenes(p_simes, level):
+
+    m = p_simes.shape[0]
+    p_sorted = np.sort(p_simes)
+    indices = np.arange(m)
+    indices_order = np.argsort(p_simes)
+    order_sig = np.max(indices[p_sorted - np.true_divide(level * (np.arange(m) + 1.), m) <= 0])
+    E_sel = indices_order[:(order_sig+1)]
+
+    return order_sig+1, E_sel
+
+def egene_selection(inpath):
+
+    allFiles = glob.glob(inpath + "/*.txt")
+    list_ = []
+    shapes = []
+    for file_ in sorted(allFiles):
+        dataArray = np.loadtxt(file_)
+        shapes.append(dataArray.shape[0])
+        list_.append(dataArray)
+    length = len(list_)
+    print("check 0: number of genes", length)
+
+    shapes = np.asarray(shapes)
+    v = np.cumsum(shapes)
+    simes_output = np.vstack(list_)
+    print("check 1: dimensions of concatenated bon outputs", simes_output.shape)
+
+    p_simes = simes_output[:, 1]
+    sig = BH_selection_egenes(p_simes, 0.10)
+
+    K = sig[0]
+    print("BH rejections", K)
+    E_sel = np.sort(sig[1])
+    print("selected indices", E_sel, E_sel.shape[0])
+    egene_p = simes_output[E_sel, 0].sum() / float(K)
+    print("average number of SNPs in selected egenes", egene_p)
+
+    return K, E_sel, v
+
+BH_output = egene_selection('/Users/snigdhapanigrahi/sim_randomized_Bon_Z/')
+E_sel = BH_output[1]
+v = BH_output[2]
+
+# outdir = '/Users/snigdhapanigrahi/sim_bon_output_liver/randomized_egenes/'
+# for i in range(v.shape[0]):
+#
+#     if i == 0:
+#         outfile = os.path.join(outdir, "egene_index_" + str(format(i+1, '03')) + ".txt")
+#         E = E_sel[(E_sel< v[0])]
+#         if E.size>0:
+#             np.savetxt(outfile, E)
+#         else:
+#             E = (-1* np.ones(1)).reshape((1,))
+#             np.savetxt(outfile, E)
+#     elif i>0 and i<length-1:
+#         outfile = os.path.join(outdir, "egene_index_" + str(format(i+1, '03')) + ".txt")
+#         E = E_sel[(E_sel>= v[i-1]) & (E_sel< v[i])]-v[i-1]
+#         if E.size>0:
+#             np.savetxt(outfile, E)
+#         else:
+#             E = (-1* np.ones(1)).reshape((1,))
+#             np.savetxt(outfile, E)
+#     elif i == length-1:
+#         outfile = os.path.join(outdir, "egene_index_" + str(format(i+1, '03')) + ".txt")
+#         E = E_sel[(E_sel>= v[i-1])]-v[i-1]
+#         if E.size > 0:
+#             np.savetxt(outfile, E)
+#         else:
+#             E = (-1 * np.ones(1)).reshape((1,))
+#             np.savetxt(outfile, E)
+
+
+# outdir = '/Users/snigdhapanigrahi/sim_bon_output_Liver/randomized_egene_boninfo/'
+# for i in range(v.shape[0]):
+#
+#     if i == 0:
+#         outfile = os.path.join(outdir, "egene_index_" + str(format(i+1, '03')) + ".txt")
+#         E = E_sel[(E_sel< v[0])]
+#         if E.size>0:
+#             np.savetxt(outfile, simes_output[E,:])
+#     elif i>0 and i<length-1:
+#         outfile = os.path.join(outdir, "egene_index_" + str(format(i+1, '03')) + ".txt")
+#         E = E_sel[(E_sel>= v[i-1]) & (E_sel< v[i])]
+#         if E.size>0:
+#             np.savetxt(outfile, simes_output[E,:])
+#     elif i == length-1:
+#         outfile = os.path.join(outdir, "egene_index_" + str(format(i+1, '03')) + ".txt")
+#         E = E_sel[(E_sel>= v[i-1])]
+#         if E.size > 0:
+#             np.savetxt(outfile, simes_output[E,:])
---
+++ selection/frequentist_eQTL/simulated_data_pipeline/sim_egene_names.py
@@ -0,0 +1,41 @@
+from __future__ import print_function
+from scipy.stats import norm as normal
+import numpy as np
+import os
+import sys
+
+path = '/Users/snigdhapanigrahi/sim_bon_output_liver/names'
+inpath = '/Users/snigdhapanigrahi/sim_bon_output_liver/randomized_egenes/'
+outpath = '/Users/snigdhapanigrahi/sim_bon_output_liver/randomized_egene_names/'
+
+for i in range(100):
+
+    gene_file = os.path.join(path + str(format(i+1, '03')) + "/Genes.txt")
+
+    with open(gene_file) as g:
+        content = g.readlines()
+
+    content = [x.strip() for x in content]
+    sys.stderr.write("length" + str(len(content)) + "\n")
+
+    index_file = np.loadtxt(os.path.join(inpath + "egene_index_" + str(format(i+1, '03')) + ".txt")).astype(int)
+
+    if index_file.size>1:
+        if index_file[0] != -1:
+            egenes = [content[index_file[k]] for k in range(index_file.shape[0])]
+            outfile = os.path.join(outpath + "egenes_" + str(format(i + 1, '03')) + ".txt")
+
+            with open(outfile, 'w') as fo:
+                for x in egenes:
+                    fo.write(str(x) + '\n')
+
+    elif index_file.size==1:
+        if index_file != -1:
+            egenes = content[index_file]
+            outfile = os.path.join(outpath + "egenes_" + str(format(i + 1, '03')) + ".txt")
+
+            with open(outfile, 'w') as fo:
+                for x in egenes:
+                    fo.write(str(x) + '\n')
+
+    sys.stderr.write("iteration completed" + str(i+1) + "\n")
