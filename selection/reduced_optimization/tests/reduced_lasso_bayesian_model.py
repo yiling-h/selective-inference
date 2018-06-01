99e0dc8ef4b4bc045264cf7d4bc8eb317b3735b4
Snigdha Panigrahi <>
2018-06-01 11:05:39 -0700

    reran LASSO for examples

--- selection/reduced_optimization/tests/reduced_lasso_bayesian_model.py
+++ selection/reduced_optimization/tests/reduced_lasso_bayesian_model.py
@@ -8,6 +8,7 @@ from selection.bayesian.initial_soln import selection, instance
 
 from selection.reduced_optimization.lasso_reduced import nonnegative_softmax_scaled, neg_log_cube_probability, selection_probability_lasso, \
     sel_prob_gradient_map_lasso, selective_inf_lasso
+from selection.tests.instance import logistic_instance, gaussian_instance
 #from selection.reduced_optimization.generative_model import generate_data
 
 class generate_data():
@@ -174,32 +175,150 @@ def randomized_lasso_trial(X,
 #              print("adjusted and unadjusted lengths", ad_len, unad_len)
 
 
+# if __name__ == "__main__":
+# # read from command line
+#     seedn=int(sys.argv[1])
+#     outdir=sys.argv[2]
+#
+#     outfile = os.path.join(outdir, "list_result_" + str(seedn) + ".txt")
+#
+# ### set parameters
+#     n = 200
+#     p = 1000
+#     s = 0
+#     snr = 5.
+#
+# ### GENERATE X
+#     np.random.seed(0)  # ensures same X
+#
+#     sample = generate_data(n, p)
+#
+# ### GENERATE Y BASED ON SEED
+#     np.random.seed(seedn) # ensures different y
+#     X, y, beta, sigma = sample.generate_response()
+#
+#     lasso = randomized_lasso_trial(X,
+#                                    y,
+#                                    beta,
+#                                    sigma)
+#
+#     np.savetxt(outfile, lasso)
+
+from rpy2 import robjects
+import rpy2.robjects.numpy2ri
+rpy2.robjects.numpy2ri.activate()
+
+def glmnet_lasso(X, y, lambda_val):
+    robjects.r('''
+                library('glmnet')
+                glmnet_LASSO = function(X,y,lambda){
+                y = as.matrix(y)
+                X = as.matrix(X)
+                lam = as.matrix(lambda)[1,1]
+                n = nrow(X)
+                fit = glmnet(X, y, standardize=FALSE, intercept=FALSE, thresh=1.e-10)
+                estimate = coef(fit, s=lam, exact=TRUE, x=X, y=y)[-1]
+                return(list(estimate = estimate))
+                }''')
+
+    lambda_R = robjects.globalenv['glmnet_LASSO']
+    n, p = X.shape
+    r_X = robjects.r.matrix(X, nrow=n, ncol=p)
+    r_y = robjects.r.matrix(y, nrow=n, ncol=1)
+    r_lam = robjects.r.matrix(lambda_val, nrow=1, ncol=1)
+    estimate = np.array(lambda_R(r_X, r_y, r_lam).rx2('estimate'))
+    return estimate
+
+def selInf_R(X, y, beta, lam, sigma, Type, alpha=0.1):
+    robjects.r('''
+               library("selectiveInference")
+               selInf = function(X, y, beta, lam, sigma, Type, alpha= 0.1){
+               y = as.matrix(y)
+               X = as.matrix(X)
+               beta = as.matrix(beta)
+               lam = as.matrix(lam)[1,1]
+               sigma = as.matrix(sigma)[1,1]
+               Type = as.matrix(Type)[1,1]
+               if(Type == 1){
+                   type = "full"} else{
+                   type = "partial"}
+               inf = fixedLassoInf(x = X, y = y, beta = beta, lambda=lam, family = "gaussian",
+                                   intercept=FALSE, sigma=sigma, alpha=alpha, type=type)
+               return(list(ci = inf$ci, pvalue = inf$pv))}
+               ''')
+
+    inf_R = robjects.globalenv['selInf']
+    n, p = X.shape
+    r_X = robjects.r.matrix(X, nrow=n, ncol=p)
+    r_y = robjects.r.matrix(y, nrow=n, ncol=1)
+    r_beta = robjects.r.matrix(beta, nrow=p, ncol=1)
+    r_lam = robjects.r.matrix(lam, nrow=1, ncol=1)
+    r_sigma = robjects.r.matrix(sigma, nrow=1, ncol=1)
+    r_Type = robjects.r.matrix(Type, nrow=1, ncol=1)
+    output = inf_R(r_X, r_y, r_beta, r_lam, r_sigma, r_Type)
+    ci = np.array(output.rx2('ci'))
+    pvalue = np.array(output.rx2('pvalue'))
+    return ci, pvalue
+
 if __name__ == "__main__":
-# read from command line
-    seedn=int(sys.argv[1])
-    outdir=sys.argv[2]
-
-    outfile = os.path.join(outdir, "list_result_" + str(seedn) + ".txt")
-
-### set parameters
+    ### set parameters
     n = 200
     p = 1000
     s = 0
     snr = 5.
 
-### GENERATE X
+    ### GENERATE X
     np.random.seed(0)  # ensures same X
 
     sample = generate_data(n, p)
 
-### GENERATE Y BASED ON SEED
-    np.random.seed(seedn) # ensures different y
-    X, y, beta, sigma = sample.generate_response()
+    niter = 50
 
-    lasso = randomized_lasso_trial(X,
-                                   y,
-                                   beta,
-                                   sigma)
+    ad_cov = 0.
+    unad_cov = 0.
+    ad_len = 0.
+    unad_len = 0.
+    prop_infty = 0.
+    nosel = 0.
 
-    np.savetxt(outfile, lasso)
+    for i in range(niter):
+
+         while True:
+             ### GENERATE Y BASED ON SEED
+             #np.random.seed(i + 1)  # ensures different y
+             #X, y, beta, sigma = sample.generate_response()
+             X, y, beta, nonzero, sigma = gaussian_instance(n=n, p=p, s=s, sigma=1., rho=0, snr=snr)
+
+             ### RUN GLMNET LASSO AND CALL SEL INFERENCE FROM R PACKAGE
+             lam = 1. * sigma * np.mean(np.fabs(np.dot(X.T, np.random.standard_normal((n, 10000)))).max(0))
+             glm_LASSO = glmnet_lasso(X, y, lam / float(n))
+             active_LASSO = (glm_LASSO != 0)
+             nactive_LASSO = active_LASSO.sum()
+             print("active variables sel by LASSO ", nactive_LASSO)
+
+             if nactive_LASSO > 0:
+                 Lee_intervals, Lee_pval = selInf_R(X, y, glm_LASSO, lam, sigma, Type=0, alpha=0.1)
+                 beta_target = np.linalg.pinv(X[:, active_LASSO]).dot(X.dot(beta))
+                 if (Lee_pval.shape[0] == beta_target.shape[0]):
+                     Lee_coverage = np.mean((beta_target > Lee_intervals[:, 0]) * (beta_target < Lee_intervals[:, 1]))
+                     inf_entries = np.isinf(Lee_intervals[:, 1] - Lee_intervals[:, 0])
+                     if inf_entries.sum() == nactive_LASSO:
+                         length_Lee = 0.
+                     else:
+                         length_Lee = np.mean((Lee_intervals[:, 1] - Lee_intervals[:, 0])[~inf_entries])
+
+                     ad_cov += Lee_coverage
+                     ad_len += length_Lee
+                     prop_infty += inf_entries.sum() / float(nactive_LASSO)
+                     print("\n")
+                     print("iteration completed", i)
+                     print("\n")
+                     print("adjusted coverage", ad_cov)
+                     print("adjusted  lengths", ad_len, prop_infty)
+                     break
+
+    print("empty draws so far ", nosel)
+    print("adjusted coverage so far", ad_cov/(50.-nosel))
+    print("adjusted lengths so far", ad_len / (50. - nosel))
+    print("prop of intervals that are infty ", prop_infty/(50. - nosel))
 
