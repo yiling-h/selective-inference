b382a238d78c6bd3260ca9ecf891c19cf48c2175
snigdhagit <psnigdha@umich.edu>
2019-04-30 15:57:51 -0400

    commit changes before switch

--- selection/bayesian/compare_inference.py
+++ selection/bayesian/compare_inference.py
@@ -1,29 +1,36 @@
-import numpy as np, sys
+import numpy as np
 from selection.randomized.lasso import lasso, selected_targets, full_targets, debiased_targets
-from selection.bayesian.utils import inference_lasso, glmnet_lasso
+from selection.bayesian.utils import glmnet_lasso, glmnet_lasso_cv1se, glmnet_lasso_cvmin, power_fdr
 from selection.bayesian.generative_instance import generate_data
+from selection.bayesian.posterior_lasso import inference_lasso
 
 def compare_inference(n= 65,
                       p= 1000,
                       sigma= 1.,
-                      rho= 0.40,
+                      rho= 0.50,
                       randomizer_scale= 1.,
-                      split_proportion= 0.60):
+                      split_proportion= 0.60,
+                      target="selected"):
 
 
     while True:
-        X, y, beta, sigma = generate_data(n=n, p=p, sigma=sigma, rho=rho, scale =True, center=True)
+        X, y, beta, sigma, scalingX = generate_data(n=n, p=p, sigma=sigma, rho=rho, scale =True, center=True)
         n, p = X.shape
+
+        true_set = np.asarray([u for u in range(p) if np.fabs(beta[u])> 1.])
+        diff_set = np.fabs(np.subtract.outer(np.arange(p), np.asarray(true_set)))
+        true_signals = np.asarray([x for x in range(p) if (min(diff_set[x, :]) <= 2).sum()>=1])
 
         if n>p:
             dispersion = np.linalg.norm(y - X.dot(np.linalg.pinv(X).dot(y))) ** 2 / (n - p)
             sigma_ = np.sqrt(dispersion)
         else:
             dispersion = None
-            sigma_ = np.std(y)
+            sigma_ = np.std(y)/np.sqrt(2.)
         print("sigma estimated and true ", sigma, sigma_)
 
-        lam_theory = sigma_ * np.mean(np.fabs(np.dot(X.T, np.random.standard_normal((n, 2000)))).max(0))
+        lam_theory = sigma_ * 0.80 * np.mean(np.fabs(np.dot(X.T, np.random.standard_normal((n, 2000)))).max(0))
+        #_, lam_1se = glmnet_lasso_cv1se(np.sqrt(n-1)*X, y)
         conv = lasso.gaussian(X,
                               y,
                               np.ones(X.shape[1]) * lam_theory,
@@ -32,16 +39,40 @@ def compare_inference(n= 65,
         signs = conv.fit()
         nonzero = signs != 0
         nreport = 0.
-        if nonzero.sum()>0:
-            beta_target = np.linalg.pinv(X[:, nonzero]).dot(X.dot(beta))
+        nactive = nonzero.sum()
 
-            (observed_target,
-             cov_target,
-             cov_target_score,
-             alternatives) = selected_targets(conv.loglike,
-                                              conv._W,
-                                              nonzero,
-                                              dispersion=dispersion)
+        if nonzero.sum()>0:
+
+            if target == "selected":
+                beta_target = np.linalg.pinv(X[:, nonzero]).dot(X.dot(beta))
+                (observed_target,
+                 cov_target,
+                 cov_target_score,
+                 alternatives) = selected_targets(conv.loglike,
+                                                  conv._W,
+                                                  nonzero,
+                                                  dispersion=dispersion)
+
+            else:
+                beta_target = beta[nonzero]
+                (observed_target,
+                 cov_target,
+                 cov_target_score,
+                 alternatives) = debiased_targets(conv.loglike,
+                                                  conv._W,
+                                                  nonzero,
+                                                  penalty=conv.penalty,
+                                                  dispersion=dispersion)
+
+            active_screenset = np.asarray([r for r in range(p) if nonzero[r]])
+            print("beta ", beta, true_set, active_screenset, beta[true_set], beta[active_screenset])
+            #print("check ", active_screenset, true_signals)
+            true_screen, false_screen = power_fdr(active_screenset, true_signals)
+            #break
+
+            power_screen = true_screen/float(true_set.sum())
+            false_screen = false_screen/float(nactive)
+
             initial_par, _, _, _, _, _ = conv.selective_MLE(observed_target,
                                                             cov_target,
                                                             cov_target_score,
@@ -61,13 +92,26 @@ def compare_inference(n= 65,
             samples = posterior_inf.posterior_sampler(nsample=2000, nburnin=50)
             lci = np.percentile(samples, 5, axis=0)
             uci = np.percentile(samples, 95, axis=0)
+
             coverage = np.mean((lci < beta_target) * (uci > beta_target))
             length = np.mean(uci - lci)
 
+            #posterior_probs = np.asarray([((np.fabs(samples[:, t])>0.10).sum())/1950. for t in range(nactive)])
+            #print("target ", posterior_probs, beta[nonzero])
+            reportind = ~((lci < 0.) * (uci > 0.))
+            #reportind = (posterior_probs>0.90)
+            reportset = np.asarray([active_screenset[a] for a in range(nactive) if reportind[a]==1])
+
+            true_total, false_total = power_fdr(reportset, true_signals)
+            power_total = true_total / float(true_set.sum())
+            false_total = false_total / max(float(true_total+false_total), 1.)
+
+            power_selective = true_total / max(float(true_screen), 1.)
+            fdr_selective = false_total/max(float(true_total + false_total), 1.)
+
         else:
             nreport = 1.
-            coverage = 0.
-            length = 0.
+            coverage, length, power_screen, false_screen, power_total, false_total, power_selective, fdr_selective =  [0., 0., 0., 0., 0., 0., 0., 0.]
 
         nreport_split = 0.
         subsample_size = int(split_proportion * n)
@@ -80,7 +124,7 @@ def compare_inference(n= 65,
         y_sel = y[sel_idx]
         X_sel = X[sel_idx, :]
         X_sel_scaled = np.sqrt(((subsample_size - 1.) / float(subsample_size)) * n) * X_sel
-        lam_theory_split = sigma_ * np.mean(
+        lam_theory_split = sigma_ * 0.80 * np.mean(
             np.fabs(np.dot(X_sel_scaled.T, np.random.standard_normal((subsample_size, 2000)))).max(0))
         glm_LASSO_split = glmnet_lasso(X_sel_scaled, y_sel, lam_theory_split / float(subsample_size))
 
@@ -88,6 +132,12 @@ def compare_inference(n= 65,
         nactive_split = active_LASSO_split.sum()
 
         if nactive_split>0:
+            active_screenset_split = np.asarray([s for s in range(p) if active_LASSO_split[s]])
+            true_screen_split, false_screen_split = power_fdr(active_screenset_split, true_signals)
+
+            power_screen_split = true_screen_split / float(true_set.sum())
+            false_screen_split = false_screen_split / float(nactive_split)
+
             noise_variance = sigma_ ** 2.
             prior_variance = 100.
             active_LASSO_split = (glm_LASSO_split != 0)
@@ -107,32 +157,48 @@ def compare_inference(n= 65,
                 (true_val_split > adjusted_intervals_split[0, :]) * (true_val_split < adjusted_intervals_split[1, :]))
             length_split = np.mean(adjusted_intervals_split[1, :] - adjusted_intervals_split[0, :])
 
+            reportind_split = ~((adjusted_intervals_split[0, :] < 0.) * (adjusted_intervals_split[1, :] > 0.))
+            reportset_split = np.asarray([active_screenset_split[b] for b in range(nactive_split) if reportind_split[b] == 1])
+
+            true_total_split, false_total_split = power_fdr(reportset_split, true_signals)
+            power_total_split = true_total_split / float(true_set.sum())
+            false_total_split = false_total_split / max(float(true_total_split + false_total_split),1.)
+
+            power_selective_split = true_total_split / max(float(true_screen_split), 1.)
+            fdr_selective_split = false_total_split / max(float(true_total_split + false_total_split), 1.)
+
         else:
             nreport_split = 1.
-            coverage_split = 0.
-            length_split = 0.
+            coverage_split, length_split, power_screen_split, false_screen_split, \
+            power_total_split, false_total_split, power_selective_split, fdr_selective_split = [0., 0., 0., 0., 0., 0., 0., 0.]
 
-        return np.vstack((coverage, length, coverage_split, length_split, nreport, nreport_split))
+        return np.vstack((coverage, length, nactive, power_screen, false_screen,
+                          power_total, false_total, power_selective, fdr_selective,
+                          coverage_split, length_split, nactive_split, power_screen_split, false_screen_split,
+                          power_total_split, false_total_split, power_selective_split, fdr_selective_split,
+                          nreport, nreport_split))
 
 
-def main(ndraw=10, split_proportion=0.80, randomizer_scale=1.):
+def main(ndraw=10, split_proportion=0.70, randomizer_scale=1.):
 
-    output = np.zeros(6)
+    output = np.zeros(20)
 
     for n in range(ndraw):
         output += np.squeeze(compare_inference(n=65,
-                                               p=1000,
+                                               p=300,
                                                sigma=1.,
-                                               rho=0.40,
+                                               rho=0.50,
                                                randomizer_scale=randomizer_scale,
-                                               split_proportion=split_proportion))
+                                               split_proportion=split_proportion,
+                                               target="debiased"))
 
         print("iteration completed ", n + 1)
+    output /= (n+1.)
+    print("inferential metrics so far ", output)
+    #print("coverage  ", output[0] / (n + 1.-output[4]), output[2] / (n + 1.-output[5]))
+    #print("lengths ", output[1] / (n + 1.-output[4]), output[3] / (n + 1.-output[5]))
 
-    print("coverage  ", output[0] / (n + 1.-output[4]), output[2] / (n + 1.-output[5]))
-    print("lengths ", output[1] / (n + 1.-output[4]), output[3] / (n + 1.-output[5]))
-
-main(ndraw=20)
+main(ndraw=10)
 
 
 
--- selection/bayesian/utils.py
+++ selection/bayesian/utils.py
@@ -1,11 +1,53 @@
-import numpy as np, sys
+import numpy as np
 
 from selection.randomized.selective_MLE_utils import solve_barrier_affine as solve_barrier_affine_C
-from scipy.stats import norm as ndist
 
 from rpy2 import robjects
 import rpy2.robjects.numpy2ri
 rpy2.robjects.numpy2ri.activate()
+
+def glmnet_lasso_cv1se(X, y):
+    robjects.r('''
+                library(glmnet)
+                glmnet_LASSO = function(X,y){
+                y = as.matrix(y)
+                X = as.matrix(X)
+                n = nrow(X)
+                fit.cv = cv.glmnet(X, y, standardize=TRUE, intercept=FALSE, thresh=1.e-10)
+                estimate.1se = coef(fit.cv, s='lambda.1se', exact=TRUE, x=X, y=y)[-1]
+                return(list(estimate.1se = estimate.1se, lam.1se = fit.cv$lambda.1se))
+                }''')
+
+    lambda_R = robjects.globalenv['glmnet_LASSO']
+    n, p = X.shape
+    r_X = robjects.r.matrix(X, nrow=n, ncol=p)
+    r_y = robjects.r.matrix(y, nrow=n, ncol=1)
+
+    estimate_1se = np.array(lambda_R(r_X, r_y).rx2('estimate.1se'))
+    lam_1se = np.asscalar(np.array(lambda_R(r_X, r_y).rx2('lam.1se')))
+    return estimate_1se, lam_1se
+
+
+def glmnet_lasso_cvmin(X, y):
+    robjects.r('''
+                library(glmnet)
+                glmnet_LASSO = function(X,y){
+                y = as.matrix(y)
+                X = as.matrix(X)
+                n = nrow(X)
+                fit.cv = cv.glmnet(X, y, standardize=TRUE, intercept=FALSE, thresh=1.e-10)
+                estimate.min = coef(fit.cv, s='lambda.min', exact=TRUE, x=X, y=y)[-1]
+                return(list(estimate.min = estimate.min,lam.min = fit.cv$lambda.min))
+                }''')
+
+    lambda_R = robjects.globalenv['glmnet_LASSO']
+    n, p = X.shape
+    r_X = robjects.r.matrix(X, nrow=n, ncol=p)
+    r_y = robjects.r.matrix(y, nrow=n, ncol=1)
+
+    estimate_min = np.array(lambda_R(r_X, r_y).rx2('estimate.min'))
+    lam_min = np.asscalar(np.array(lambda_R(r_X, r_y).rx2('lam.min')))
+    return estimate_min, lam_min
 
 def glmnet_lasso(X, y, lambda_val):
     robjects.r('''
@@ -75,159 +117,26 @@ def log_likelihood(target_parameter,
 
     return log_lik
 
-class projected_langevin(object):
+def coverage(intervals, target, truth):
 
-    def __init__(self,
-                 initial_condition,
-                 gradient_map,
-                 stepsize):
+    cov = (0. > intervals[:, 0]) * (0. < intervals[:, 1])
+    pval_alt = (cov[truth != 0]) != 1
+    if pval_alt.sum() > 0:
+        avg_power = np.mean(pval_alt)
+    else:
+        avg_power = 0.
+    return np.mean((target > intervals[:, 0]) * (target < intervals[:, 1])), avg_power
 
-        (self.state,
-         self.gradient_map,
-         self.stepsize) = (np.copy(initial_condition),
-                           gradient_map,
-                           stepsize)
-        self._shape = self.state.shape[0]
-        self._sqrt_step = np.sqrt(self.stepsize)
-        self._noise = ndist(loc=0,scale=1)
-        self.sample = np.copy(initial_condition)
+def power_fdr(select_set, true_set):
+    if select_set.shape[0] > 0 and true_set.shape[0] > 0:
+        diff_set = np.fabs(np.subtract.outer(select_set, true_set))
+        #print("check diff ", diff_set, diff_set.shape)
+        true_discoveries = (diff_set.min(0) == 0).sum()
+        false_discoveries = select_set.shape[0] - true_discoveries
+        return true_discoveries, false_discoveries
 
-    def __iter__(self):
-        return self
-
-    def next(self):
-        while True:
-            grad_posterior = self.gradient_map(self.state)
-            candidate = (self.state + self.stepsize * grad_posterior[0]
-                        + np.sqrt(2.)* self._noise.rvs(self._shape) * self._sqrt_step)
-            if not np.all(np.isfinite(self.gradient_map(candidate)[0])):
-                print(candidate, self._sqrt_step)
-                self._sqrt_step *= 0.8
-            else:
-                self.state[:] = candidate
-                self.sample[:] = grad_posterior[1]
-                print(" next sample ", self.state[:], self.sample[:])
-                break
-
-class inference_lasso():
-
-    def __init__(self,
-                 observed_target,
-                 cov_target,
-                 cov_target_score,
-                 feasible_point,
-                 cond_mean,
-                 cond_cov,
-                 logdens_linear,
-                 linear_part,
-                 offset,
-                 ini_estimate):
-
-        self.observed_target = observed_target
-        self.cov_target = cov_target
-        self.cov_target_score = cov_target_score
-        self.feasible_point = feasible_point
-        self.cond_mean = cond_mean
-        self.cond_cov = cond_cov
-        self.target_size = cond_cov.shape[0]
-        self.logdens_linear = logdens_linear
-        self.linear_part = linear_part
-        self.offset = offset
-        self.ini_estimate = ini_estimate
-
-    def det_initial_point(self, solve_args={'tol':1.e-12}):
-
-        if np.asarray(self.observed_target).shape in [(), (0,)]:
-            raise ValueError('no target specified')
-
-        observed_target = np.atleast_1d(self.observed_target)
-        prec_target = np.linalg.inv(self.cov_target)
-
-        target_lin = - self.logdens_linear.dot(self.cov_target_score.T.dot(prec_target))
-        target_offset = self.cond_mean - target_lin.dot(observed_target)
-
-        prec_opt = np.linalg.inv(self.cond_cov)
-        mean_opt = target_lin.dot(self.ini_estimate) + target_offset
-        conjugate_arg = prec_opt.dot(mean_opt)
-
-        solver = solve_barrier_affine_C
-
-        val, soln, hess = solver(conjugate_arg,
-                                 prec_opt,
-                                 self.feasible_point,
-                                 self.linear_part,
-                                 self.offset,
-                                 **solve_args)
-
-        initial_point = self.ini_estimate + self.cov_target.dot(target_lin.T.dot(prec_opt.dot(mean_opt - soln)))
-        return initial_point
-
-    def gradient_log_likelihood(self, target_parameter, solve_args={'tol':1.e-12}):
-
-        if np.asarray(self.observed_target).shape in [(), (0,)]:
-            raise ValueError('no target specified')
-
-        observed_target = np.atleast_1d(self.observed_target)
-        prec_target = np.linalg.inv(self.cov_target)
-
-        target_lin = - self.logdens_linear.dot(self.cov_target_score.T.dot(prec_target))
-        target_offset = self.cond_mean - target_lin.dot(observed_target)
-
-        prec_opt = np.linalg.inv(self.cond_cov)
-        mean_opt = target_lin.dot(target_parameter) + target_offset
-        conjugate_arg = prec_opt.dot(mean_opt)
-
-        solver = solve_barrier_affine_C
-
-        val, soln, hess = solver(conjugate_arg,
-                                 prec_opt,
-                                 self.feasible_point,
-                                 self.linear_part,
-                                 self.offset,
-                                 **solve_args)
-
-        reparam = target_parameter + self.cov_target.dot(target_lin.T.dot(prec_opt.dot(mean_opt - soln)))
-
-        grad_barrier = np.diag(2. / ((1. + soln) ** 3.) - 2. / (soln ** 3.))
-
-        L = target_lin.T.dot(prec_opt)
-        N = L.dot(hess)
-        jacobian = (np.identity(observed_target.shape[0]) + self.cov_target.dot(L).dot(target_lin)) - \
-                   self.cov_target.dot(N).dot(L.T)
-
-        grad_lik = jacobian.T.dot(prec_target).dot(observed_target)
-        grad_neg_normalizer = -jacobian.T.dot(prec_target).dot(target_parameter)
-
-        opt_num = self.cond_cov.shape[0]
-        grad_jacobian = np.zeros(opt_num)
-        A = np.linalg.inv(jacobian).dot(self.cov_target).dot(N)
-        for j in range(opt_num):
-            M = grad_barrier.dot(np.diag(N.T[:, j]))
-            grad_jacobian[j] = np.trace(A.dot(M).dot(N.T))
-
-        return grad_lik + grad_neg_normalizer + grad_jacobian -target_parameter/100., reparam
-
-    def posterior_sampler(self, nsample= 2000, nburnin=100):
-
-        initial_state = self.det_initial_point()
-        state = initial_state
-
-        stepsize = 1. / (1 * self.target_size)
-        sampler = projected_langevin(state, self.gradient_log_likelihood, stepsize)
-
-        samples = []
-
-        for i in range(nsample):
-            sampler.next()
-            samples.append(sampler.sample.copy())
-            sys.stderr.write("sample number: " + str(i) + "\n")
-
-        samples = np.array(samples)
-        return samples[nburnin:, :]
-
-
-
-
+    else:
+        return 0., 0.
 
 
 
