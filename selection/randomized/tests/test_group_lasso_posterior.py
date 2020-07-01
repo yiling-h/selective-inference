fixed gradient of log-normalizer

selection/randomized/tests/test_group_lasso_posterior.py
@@ -94,13 +94,13 @@ def main(ndraw=10):
     for n in range(ndraw):
         cov, len = test_posterior_inference(n=500,
                                             p=200,
-                                            signal_fac=1.2,
+                                            signal_fac=1.,
                                             sgroup=3,
                                             s=5,
                                             groups=np.arange(50).repeat(4),
-                                            sigma=3.,
+                                            sigma=1.,
                                             rho=0.20,
-                                            randomizer_scale=1.,
+                                            randomizer_scale=0.5,
                                             weight_frac=1.2)
 
         coverage_ += cov