f28b887c4b7627e85a87710414b5792116a46967
Dan Kessler <kesslerd@umich.edu>
2020-08-17 22:31:34 -0400

    fix path

--- selection/randomized/experiments/posterior-analyze.py
+++ selection/randomized/experiments/posterior-analyze.py
@@ -5,8 +5,8 @@ import numpy as np
 
 traj = Trajectory('CoverageChecks')
 
-traj.f_load(filename='./selection/randomized/experiments/hdf5/CoverageChecks.hdf5',
-            load_results=1, load_parameters=2)
+traj.f_load(filename='./hdf5/CoverageChecks.hdf5',
+            load_results=2, load_parameters=2)
 
 coverage = traj.f_get_from_runs(name='mean.coverage', fast_access=True, auto_load=True, shortcuts=False)
 
