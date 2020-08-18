f06f0e0908ffb5cb7ce1df4302a2c3ab9ec80834
Dan Kessler <kesslerd@umich.edu>
2020-08-17 22:48:04 -0400

    use pandas to explore simulation results

--- selection/randomized/experiments/posterior-analyze.py
+++ selection/randomized/experiments/posterior-analyze.py
@@ -2,6 +2,7 @@ from pypet import Trajectory
 import matplotlib.pyplot as plt
 import matplotlib
 import numpy as np
+import pandas as pd
 
 traj = Trajectory('CoverageChecks')
 
@@ -13,11 +14,19 @@ coverage = traj.f_get_from_runs(name='mean.coverage', fast_access=True, auto_loa
 length = traj.f_get_from_runs(name='mean.length', fast_access=True, auto_load = True, shortcuts = False)
 
 sgroup = traj.f_get('sgroup').f_get_range()
-signal = traj.f_get('signal').f_get_range()
+signal_fac = traj.f_get('signal_fac').f_get_range()
+
+df = pd.DataFrame({'SGroup': sgroup,
+                   'Signal': signal_fac,
+                   'coverage': list(coverage.values()),
+                   'length': list(length.values())})
+
+df.groupby(['SGroup', 'Signal']).mean()
+df.groupby(['SGroup', 'Signal']).var()
 
 for sg in np.unique(sgroup):
     mask = np.equal(sg, sgroup)
-    plt.plot(np.array(signal)[mask], np.array(list(coverage.values()))[mask], label = 'Number Active Groups: %s' % str(sg))
+    plt.plot(np.array(signal_fac)[mask], np.array(list(coverage.values()))[mask], label = 'Number Active Groups: %s' % str(sg))
 
 plt.xlabel('Signal')
 plt.ylabel('Coverage')
