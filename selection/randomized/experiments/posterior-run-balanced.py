f4d2a1de1138e93a565bb0ef2ce28ee5527460c0
Dan Kessler <kesslerd@umich.edu>
2020-11-09 20:24:20 -0500

    crank up the sims

--- selection/randomized/experiments/posterior-run-balanced.py
+++ selection/randomized/experiments/posterior-run-balanced.py
@@ -232,4 +232,4 @@ def main(nreps=1):
 if __name__ == '__main__':
     # Let's make the python evangelists happy and encapsulate
     # the main function as you always should ;-)
-    main(10)
+    main(100)
