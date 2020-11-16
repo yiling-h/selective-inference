890432f5e7085ec54cfb8df293234cbf088a6506
Dan Kessler <kesslerd@umich.edu>
2020-11-16 14:25:29 -0500

    update visualization script

--- selection/randomized/experiments/hdf5/visualize.R
+++ selection/randomized/experiments/hdf5/visualize.R
@@ -1,24 +1,55 @@
 library(ggplot2)
 library(tidyr)
 
-dat.wide <- read.csv('posterior-balanced.csv')
+## balanced study
+bal.wide <- read.csv('posterior-balanced.csv')
 
-dat <- tidyr::pivot_longer(
-                       dat.wide,
+bal <- tidyr::pivot_longer(
+                       bal.wide,
                        cols = coverage_naive:length_posi,
                        names_to = c('metric','method'),
                        names_sep = '_')
 
-dat[dat$metric == 'coverage',] %>%
+bal.cov <- bal[bal$metric == 'coverage',] %>%
     ggplot(aes(x = Signal, y = value, color = method, style = method)) +
     stat_summary(fun.data = mean_se, geom = 'errorbar', size = 2) +
     stat_summary(fun.data = mean_se, geom = 'line', size = 2) +
     theme_bw(base_size = 30) + 
     ylab('Coverage')
 
-dat[dat$metric == 'length',] %>%
+bal.len <- bal[bal$metric == 'length',] %>%
     ggplot(aes(x = Signal, y = value, color = method, style = method)) +
     stat_summary(fun.data = mean_se, geom = 'errorbar', size = 2 ) +
     stat_summary(fun.data = mean_se, geom = 'line', size = 2) +
     theme_bw(base_size = 30) + 
     ylab('Length')
+
+ggsave('bal-cov.png', bal.cov, width = 19.20, height = 10.80, units = 'in')
+ggsave('bal-len.png', bal.len, width = 19.20, height = 10.80, units = 'in')
+
+## het study
+het.wide <- read.csv('posterior-hetero.csv')
+
+het <- tidyr::pivot_longer(
+                       het.wide,
+                       cols = coverage_naive:length_posi,
+                       names_to = c('metric','method'),
+                       names_sep = '_')
+
+het.cov <- het[het$metric == 'coverage',] %>%
+    ggplot(aes(x = Signal_Lower, y = value, color = method, style = method)) +
+    stat_summary(fun.data = mean_se, geom = 'errorbar', size = 2) +
+    stat_summary(fun.data = mean_se, geom = 'line', size = 2) +
+    theme_bw(base_size = 30) + 
+    ylab('Coverage')
+
+het.len <- het[het$metric == 'length',] %>%
+    ggplot(aes(x = Signal_Lower, y = value, color = method, style = method)) +
+    stat_summary(fun.data = mean_se, geom = 'errorbar', size = 2 ) +
+    stat_summary(fun.data = mean_se, geom = 'line', size = 2) +
+    theme_bw(base_size = 30) + 
+    ylab('Length')
+
+ggsave('het-cov.png', het.cov, width = 19.20, height = 10.80, units = 'in')
+ggsave('het-len.png', het.len, width = 19.20, height = 10.80, units = 'in')
+
