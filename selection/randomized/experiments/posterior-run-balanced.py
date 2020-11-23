4633b75db5b4cd5118694bdd7f9a3a6f3088efe0
Dan Kessler <kesslerd@umich.edu>
2020-11-23 11:02:08 -0500

    explore signal_fac from 0.5 to 1.5

--- selection/randomized/experiments/posterior-run-balanced.py
+++ selection/randomized/experiments/posterior-run-balanced.py
@@ -32,7 +32,7 @@ def main(nreps=1):
     seeds = [19860 + i for i in range(nreps)]  # offset seed for each rep
 
     # specify parameters to explore
-    traj.f_explore(cartesian_product({"signal_fac": np.arange(0.1, 2, 0.1),
+    traj.f_explore(cartesian_product({"signal_fac": np.linspace(0.5, 1.5, 10),
                                       'sgroup': [3],
                                       'seed': seeds}))
 
