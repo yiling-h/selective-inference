5c122301319f7eeedb3afac553a35d84272b2e89
Dan Kessler <kesslerd@umich.edu>
2020-11-23 13:19:46 -0500

    setup linspace to hit nice increments

--- selection/randomized/experiments/posterior-run-hetero.py
+++ selection/randomized/experiments/posterior-run-hetero.py
@@ -32,7 +32,7 @@ def main(nreps=1):
     seeds = [19860 + i for i in range(nreps)]  # offset seed for each rep
 
     # specify parameters to explore
-    traj.f_explore(cartesian_product({"signal_fac": [(i, i+1) for i in np.linspace(0.5, 1.5, 10)],
+    traj.f_explore(cartesian_product({"signal_fac": [(i, i+1) for i in np.linspace(0.5, 1.5, 11)],
                                       'seed': seeds}))
 
     env.run(coverage_experiment)
