a8efa0e17689e00ec71de74794ff03258712abac
Dan Kessler <kesslerd@umich.edu>
2020-11-23 14:47:41 -0500

    add plots for atomic case

--- selection/randomized/experiments/hdf5/visualize.R
+++ selection/randomized/experiments/hdf5/visualize.R
@@ -1,6 +1,32 @@
 library(ggplot2)
 library(tidyr)
 
+## atomic study
+atom.wide <- read.csv('posterior-atomic.csv')
+
+atom <- tidyr::pivot_longer(
+                       atom.wide,
+                       cols = coverage_naive:length_posi,
+                       names_to = c('metric','method'),
+                       names_sep = '_')
+
+atom.cov <- atom[atom$metric == 'coverage',] %>%
+    ggplot(aes(x = Signal, y = value, color = method, style = method)) +
+    stat_summary(fun.data = mean_se, geom = 'errorbar', size = 2, position=position_dodge(width=0.05)) +
+    stat_summary(fun.data = mean_se, geom = 'line', size = 2, position = position_dodge(width = 0.05)) +
+    theme_bw(base_size = 30) + 
+    ylab('Coverage')
+
+atom.len <- atom[atom$metric == 'length',] %>%
+    ggplot(aes(x = Signal, y = value, color = method, style = method)) +
+    stat_summary(fun.data = mean_se, geom = 'errorbar', size = 2 ) +
+    stat_summary(fun.data = mean_se, geom = 'line', size = 2) +
+    theme_bw(base_size = 30) + 
+    ylab('Length')
+
+ggsave('atom-cov.png', atom.cov, width = 19.20, height = 10.80, units = 'in')
+ggsave('atom-len.png', atom.len, width = 19.20, height = 10.80, units = 'in')
+
 ## balanced study
 bal.wide <- read.csv('posterior-balanced.csv')
 
