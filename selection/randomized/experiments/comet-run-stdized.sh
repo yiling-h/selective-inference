d9562cc155e8f60c018f53dda5dcb467ca64c483
Dan Kessler <kesslerd@umich.edu>
2020-11-23 14:21:50 -0500

    add comet launch script for stdized

---
+++ selection/randomized/experiments/comet-run-stdized.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#SBATCH --job-name="oposi-std"
+#SBATCH --partition=compute
+#SBATCH --nodes=23
+#SBATCH --ntasks-per-node=24
+#SBATCH -t 01:00:00
+#SBATCH -A TG-DMS190038
+
+python -m scoop posterior-run-stdized.py
