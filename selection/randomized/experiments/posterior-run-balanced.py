e1d0d3fb235db0852e697eb2853541d5171829e1
Dan Kessler <kesslerd@umich.edu>
2020-11-23 11:02:01 -0500

    use same seed offset everywhere

--- selection/randomized/experiments/posterior-run-balanced.py
+++ selection/randomized/experiments/posterior-run-balanced.py
@@ -29,7 +29,7 @@ def main(nreps=1):
     traj.f_add_parameter('weight_frac', 1.0)
     traj.f_add_parameter('seed', 0)  # random seed
 
-    seeds = [1986 + i for i in range(nreps)]  # offset seed for each rep
+    seeds = [19860 + i for i in range(nreps)]  # offset seed for each rep
 
     # specify parameters to explore
     traj.f_explore(cartesian_product({"signal_fac": np.arange(0.1, 2, 0.1),
