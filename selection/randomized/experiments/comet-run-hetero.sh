eff35431240ad2ea537eea5713d6dfad8723dedb
Dan Kessler <kesslerd@umich.edu>
2020-11-23 13:19:00 -0500

    rename comet run file for hetero case

--- selection/randomized/experiments/comet-run-hetero.sh
+++ selection/randomized/experiments/comet-run-hetero.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-#SBATCH --job-name="posi-bal"
+#SBATCH --job-name="posi-het"
 #SBATCH --partition=compute
-#SBATCH --nodes=40
+#SBATCH --nodes=46
 #SBATCH --ntasks-per-node=24
 #SBATCH -t 01:00:00
 #SBATCH -A TG-DMS190038
 
-python -m scoop posterior-run-balanced.py
+python -m scoop posterior-run-hetero.py
