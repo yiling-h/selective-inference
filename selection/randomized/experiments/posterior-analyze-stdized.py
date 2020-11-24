d3f69e0b112fb13940cb5b40c8c911fda7dff2a3
Dan Kessler <kesslerd@umich.edu>
2020-11-24 06:22:40 -0800

    add stdized results

---
+++ selection/randomized/experiments/posterior-analyze-stdized.py
@@ -0,0 +1,29 @@
+from pypet import Trajectory
+import matplotlib.pyplot as plt
+import matplotlib
+import numpy as np
+import pandas as pd
+
+traj = Trajectory('GrpLasso_Std_Balanced')
+
+traj.f_load(filename='./selection/randomized/experiments/hdf5/GrpLasso_Std_Balanced.hdf5',
+            load_results=1, load_parameters=2)
+
+coverage_posi = list(traj.f_get_from_runs(name='posi.mean.coverage', fast_access=True, auto_load=True, shortcuts=False).values())
+length_posi = list(traj.f_get_from_runs(name='posi.mean.length', fast_access=True, auto_load=True, shortcuts=False).values())
+coverage_naive = list(traj.f_get_from_runs(name='naive.mean.coverage', fast_access=True, auto_load=True, shortcuts=False).values())
+length_naive = list(traj.f_get_from_runs(name='naive.mean.length', fast_access=True, auto_load=True, shortcuts=False).values())
+coverage_split = list(traj.f_get_from_runs(name='split.mean.coverage', fast_access=True, auto_load=True, shortcuts=False).values())
+length_split = list(traj.f_get_from_runs(name='split.mean.length', fast_access=True, auto_load=True, shortcuts=False).values())
+
+signal_fac = traj.f_get('signal_fac').f_get_range()
+
+df = pd.DataFrame({'Signal': signal_fac,
+                   'coverage_naive': coverage_naive,
+                   'coverage_split': coverage_split,
+                   'coverage_posi': coverage_posi,
+                   'length_naive': length_naive,
+                   'length_split': length_split,
+                   'length_posi': length_posi})
+
+df.to_csv('selection/randomized/experiments/hdf5/posterior-stdized.csv')
