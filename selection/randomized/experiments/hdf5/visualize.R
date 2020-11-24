04cf957dfa7f1b6e975c0b5aabce2568b2330d7b
Dan Kessler <kesslerd@umich.edu>
2020-11-24 10:20:11 -0500

    visualize standardized results, too

--- selection/randomized/experiments/hdf5/visualize.R
+++ selection/randomized/experiments/hdf5/visualize.R
@@ -79,3 +79,28 @@ het.len <- het[het$metric == 'length',] %>%
 ggsave('het-cov.png', het.cov, width = 19.20, height = 10.80, units = 'in')
 ggsave('het-len.png', het.len, width = 19.20, height = 10.80, units = 'in')
 
+## standardized study
+std.wide <- read.csv('posterior-stdized.csv')
+
+std <- tidyr::pivot_longer(
+                       std.wide,
+                       cols = coverage_naive:length_posi,
+                       names_to = c('metric','method'),
+                       names_sep = '_')
+
+std.cov <- std[std$metric == 'coverage',] %>%
+    ggplot(aes(x = Signal, y = value, color = method, style = method)) +
+    stat_summary(fun.data = mean_se, geom = 'errorbar', size = 2, position=position_dodge(width=0.05)) +
+    stat_summary(fun.data = mean_se, geom = 'line', size = 2, position = position_dodge(width = 0.05)) +
+    theme_bw(base_size = 30) + 
+    ylab('Coverage')
+
+std.len <- std[std$metric == 'length',] %>%
+    ggplot(aes(x = Signal, y = value, color = method, style = method)) +
+    stat_summary(fun.data = mean_se, geom = 'errorbar', size = 2 ) +
+    stat_summary(fun.data = mean_se, geom = 'line', size = 2) +
+    theme_bw(base_size = 30) + 
+    ylab('Length')
+
+ggsave('std-cov.png', std.cov, width = 19.20, height = 10.80, units = 'in')
+ggsave('std-len.png', std.len, width = 19.20, height = 10.80, units = 'in')
