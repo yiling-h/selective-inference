abf7da5dd82f3b2fbf4cf9c1674a840231e29d9a
Dan Kessler <kesslerd@umich.edu>
2020-11-30 12:53:59 -0800

    delete old launch script

--- selection/randomized/experiments/comet-posterior-run-hetero.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-#SBATCH --job-name="oposi-het"
-#SBATCH --partition=compute
-#SBATCH --nodes=40
-#SBATCH --ntasks-per-node=24
-#SBATCH -t 01:00:00
-#SBATCH -A TG-DMS190038
-
-python -m scoop posterior-run-hetero.py
