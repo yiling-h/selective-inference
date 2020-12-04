33cc98ff207d2349f9da48ee28086b7b56b9f9fd
Dan Kessler <kesslerd@umich.edu>
2020-12-04 10:04:41 -0500

    give all experiments 8 hours to run
    and create OG launch script

--- selection/randomized/experiments/comet-run-balanced.sh
+++ selection/randomized/experiments/comet-run-balanced.sh
@@ -3,7 +3,7 @@
 #SBATCH --partition=compute
 #SBATCH --nodes=32
 #SBATCH --ntasks-per-node=24
-#SBATCH -t 01:00:00
+#SBATCH -t 08:00:00
 #SBATCH -A TG-DMS190038
 
 python -m scoop posterior-run-balanced.py
--- selection/randomized/experiments/comet-run-og.sh
+++ selection/randomized/experiments/comet-run-og.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-#SBATCH --job-name="oposi-het"
+#SBATCH --job-name="oposi-og"
 #SBATCH --partition=compute
 #SBATCH --nodes=32
 #SBATCH --ntasks-per-node=24
 #SBATCH -t 08:00:00
 #SBATCH -A TG-DMS190038
 
-python -m scoop posterior-run-atomic.py
+python -m scoop posterior-run-og.py
