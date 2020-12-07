29a7afbabb39c54d16a94dcd1f69283635dfb5ed
Dan Kessler <kesslerd@umich.edu>
2020-12-07 00:35:36 -0500

    add gll launcher versions for the experiments
    same setup as comet, just different account and request
    distribution (on greatlakes, we can just request tasks and let the
    system spread it out over nodes since we are billed per core, whereas
    on the comet partition we were using you're billed per node)

---
+++ selection/randomized/experiments/gll-run-hetero.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#SBATCH --job-name="posi-het"
+#SBATCH --ntasks=750
+#SBATCH --mem-per-cpu=6gb
+#SBATCH --time=8:00:00
+#SBATCH --account=stats_dept1
+#SBATCH --partition=standard
+# The application(s) to execute along with its input arguments and options:
+
+python -m scoop posterior-run-hetero.py
