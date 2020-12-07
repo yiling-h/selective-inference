4746d6d5a24fb709653ae9af9979c770bfde69d2
Dan Kessler <kesslerd@umich.edu>
2020-12-07 12:40:12 -0500

    add sigdet plots to visualizations
    won't work for hetero yet, since we haven't rerun with those results

--- selection/randomized/experiments/hdf5/visualize.R
+++ selection/randomized/experiments/hdf5/visualize.R
@@ -6,7 +6,7 @@ atom.wide <- read.csv('posterior-atomic.csv')
 
 atom <- tidyr::pivot_longer(
                        atom.wide,
-                       cols = coverage_naive:length_posi,
+                       cols = coverage_naive:fn_posi,
                        names_to = c('metric','method'),
                        names_sep = '_')
 
@@ -24,15 +24,26 @@ atom.len <- atom[atom$metric == 'length',] %>%
     theme_bw(base_size = 30) + 
     ylab('Length')
 
+
+atom.sigdet <- atom[atom$metric %in% c('tp','fp','tn','fn'),] %>%
+    ggplot(aes(x = Signal, y = value, color = method, style = method)) +
+    stat_summary(fun.data = mean_se, geom = 'errorbar', size = 2 ) +
+    stat_summary(fun.data = mean_se, geom = 'line', size = 2) +
+    facet_wrap(~metric, scales='free_y') +
+    theme_bw(base_size = 30) +
+    ylab('Count')
+
+
 ggsave('atom-cov.png', atom.cov, width = 19.20, height = 10.80, units = 'in')
 ggsave('atom-len.png', atom.len, width = 19.20, height = 10.80, units = 'in')
+ggsave('atom-sigdet.png', atom.sigdet, width = 19.20, height = 10.80, units = 'in')
 
 ## balanced study
 bal.wide <- read.csv('posterior-balanced.csv')
 
 bal <- tidyr::pivot_longer(
                        bal.wide,
-                       cols = coverage_naive:length_posi,
+                       cols = coverage_naive:fn_posi,
                        names_to = c('metric','method'),
                        names_sep = '_')
 
@@ -50,15 +61,26 @@ bal.len <- bal[bal$metric == 'length',] %>%
     theme_bw(base_size = 30) + 
     ylab('Length')
 
+
+bal.sigdet <- bal[bal$metric %in% c('tp','fp','tn','fn'),] %>%
+    ggplot(aes(x = Signal, y = value, color = method, style = method)) +
+    stat_summary(fun.data = mean_se, geom = 'errorbar', size = 2 ) +
+    stat_summary(fun.data = mean_se, geom = 'line', size = 2) +
+    facet_wrap(~metric, scales='free_y') +
+    theme_bw(base_size = 30) +
+    ylab('Count')
+
+
 ggsave('bal-cov.png', bal.cov, width = 19.20, height = 10.80, units = 'in')
 ggsave('bal-len.png', bal.len, width = 19.20, height = 10.80, units = 'in')
+ggsave('bal-sigdet.png', bal.sigdet, width = 19.20, height = 10.80, units = 'in')
 
 ## het study
 het.wide <- read.csv('posterior-hetero.csv')
 
 het <- tidyr::pivot_longer(
                        het.wide,
-                       cols = coverage_naive:length_posi,
+                       cols = coverage_naive:fn_posi,
                        names_to = c('metric','method'),
                        names_sep = '_')
 
@@ -76,15 +98,26 @@ het.len <- het[het$metric == 'length',] %>%
     theme_bw(base_size = 30) + 
     ylab('Length')
 
+
+het.sigdet <- het[het$metric %in% c('tp','fp','tn','fn'),] %>%
+    ggplot(aes(x = Signal, y = value, color = method, style = method)) +
+    stat_summary(fun.data = mean_se, geom = 'errorbar', size = 2 ) +
+    stat_summary(fun.data = mean_se, geom = 'line', size = 2) +
+    facet_wrap(~metric, scales='free_y') +
+    theme_bw(base_size = 30) +
+    ylab('Count')
+
+
 ggsave('het-cov.png', het.cov, width = 19.20, height = 10.80, units = 'in')
 ggsave('het-len.png', het.len, width = 19.20, height = 10.80, units = 'in')
+ggsave('het-sigdet.png', het.sigdet, width = 19.20, height = 10.80, units = 'in')
 
 ## standardized study
 std.wide <- read.csv('posterior-stdized.csv')
 
 std <- tidyr::pivot_longer(
                        std.wide,
-                       cols = coverage_naive:length_posi,
+                       cols = coverage_naive:fn_posi,
                        names_to = c('metric','method'),
                        names_sep = '_')
 
@@ -102,15 +135,24 @@ std.len <- std[std$metric == 'length',] %>%
     theme_bw(base_size = 30) + 
     ylab('Length')
 
+std.sigdet <- std[std$metric %in% c('tp','fp','tn','fn'),] %>%
+    ggplot(aes(x = Signal, y = value, color = method, style = method)) +
+    stat_summary(fun.data = mean_se, geom = 'errorbar', size = 2 ) +
+    stat_summary(fun.data = mean_se, geom = 'line', size = 2) +
+    facet_wrap(~metric, scales='free_y') +
+    theme_bw(base_size = 30) +
+    ylab('Count')
+
 ggsave('std-cov.png', std.cov, width = 19.20, height = 10.80, units = 'in')
 ggsave('std-len.png', std.len, width = 19.20, height = 10.80, units = 'in')
+ggsave('std-sigdet.png', std.sigdet, width = 19.20, height = 10.80, units = 'in')
 
 ## og study
 og.wide <- read.csv('posterior-og.csv')
 
 og <- tidyr::pivot_longer(
                        og.wide,
-                       cols = coverage_naive:length_posi,
+                       cols = coverage_naive:fn_posi,
                        names_to = c('metric','method'),
                        names_sep = '_')
 
@@ -128,5 +170,16 @@ og.len <- og[og$metric == 'length',] %>%
     theme_bw(base_size = 30) +
     ylab('Length')
 
+
+og.sigdet <- og[og$metric %in% c('tp','fp','tn','fn'),] %>%
+    ggplot(aes(x = Signal, y = value, color = method, style = method)) +
+    stat_summary(fun.data = mean_se, geom = 'errorbar', size = 2 ) +
+    stat_summary(fun.data = mean_se, geom = 'line', size = 2) +
+    facet_wrap(~metric, scales='free_y') +
+    theme_bw(base_size = 30) +
+    ylab('Count')
+
+
 ggsave('og-cov.png', og.cov, width = 19.20, height = 10.80, units = 'in')
 ggsave('og-len.png', og.len, width = 19.20, height = 10.80, units = 'in')
+ggsave('og-sigdet.png', og.sigdet, width = 19.20, height = 10.80, units = 'in')
