5ecae0d103e17a20acf08bd9a7e5219d9c1dbf70
Dan Kessler <kesslerd@umich.edu>
2020-12-23 21:38:39 -0800

    atomic should run very fast, so use just a few nodes for an hour

--- selection/randomized/experiments/comet-run-atomic.sh
+++ selection/randomized/experiments/comet-run-atomic.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 #SBATCH --job-name="posi-atom"
 #SBATCH --partition=compute
-#SBATCH --nodes=12
+#SBATCH --nodes=2
 #SBATCH --ntasks-per-node=24
-#SBATCH -t 08:00:00
+#SBATCH -t 01:00:00
 #SBATCH -A TG-DMS190038
 
 python -m scoop posterior-run-atomic.py
