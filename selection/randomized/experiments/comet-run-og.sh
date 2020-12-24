cc80086664b992638690de70b4e95710da192bb9
Dan Kessler <kesslerd@umich.edu>
2020-12-23 21:40:53 -0800

    scale back og to use 100 sims and fewer resources
    we might run out of time, but if we hit that limit then the experiment
    isn't well tuned

--- selection/randomized/experiments/comet-run-og.sh
+++ selection/randomized/experiments/comet-run-og.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 #SBATCH --job-name="posi-og"
 #SBATCH --partition=compute
-#SBATCH --nodes=32
+#SBATCH --nodes=6
 #SBATCH --ntasks-per-node=24
-#SBATCH -t 08:00:00
+#SBATCH -t 02:00:00
 #SBATCH -A TG-DMS190038
 
 python -m scoop posterior-run-og.py
