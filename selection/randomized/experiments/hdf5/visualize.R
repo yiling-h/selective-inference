fe82a1e021aba9697ecc0508cd4ba06b35440fb7
Dan Kessler <kesslerd@umich.edu>
2020-12-24 15:47:14 -0500

    don't write out individual figures for each setting
    use the compact plots instead

--- selection/randomized/experiments/hdf5/visualize.R
+++ selection/randomized/experiments/hdf5/visualize.R
@@ -34,9 +34,9 @@ atom.sigdet <- atom[atom$metric %in% c('tp','fp','tn','fn'),] %>%
     ylab('Count')
 
 
-ggsave('atom-cov.png', atom.cov, width = 19.20, height = 10.80, units = 'in')
-ggsave('atom-len.png', atom.len, width = 19.20, height = 10.80, units = 'in')
-ggsave('atom-sigdet.png', atom.sigdet, width = 19.20, height = 10.80, units = 'in')
+## ggsave('atom-cov.png', atom.cov, width = 19.20, height = 10.80, units = 'in')
+## ggsave('atom-len.png', atom.len, width = 19.20, height = 10.80, units = 'in')
+## ggsave('atom-sigdet.png', atom.sigdet, width = 19.20, height = 10.80, units = 'in')
 
 ## balanced study
 bal.wide <- read.csv('posterior-balanced.csv')
@@ -71,9 +71,9 @@ bal.sigdet <- bal[bal$metric %in% c('tp','fp','tn','fn'),] %>%
     ylab('Count')
 
 
-ggsave('bal-cov.png', bal.cov, width = 19.20, height = 10.80, units = 'in')
-ggsave('bal-len.png', bal.len, width = 19.20, height = 10.80, units = 'in')
-ggsave('bal-sigdet.png', bal.sigdet, width = 19.20, height = 10.80, units = 'in')
+## ggsave('bal-cov.png', bal.cov, width = 19.20, height = 10.80, units = 'in')
+## ggsave('bal-len.png', bal.len, width = 19.20, height = 10.80, units = 'in')
+## ggsave('bal-sigdet.png', bal.sigdet, width = 19.20, height = 10.80, units = 'in')
 
 ## het study
 het.wide <- read.csv('posterior-hetero.csv')
@@ -108,9 +108,9 @@ het.sigdet <- het[het$metric %in% c('tp','fp','tn','fn'),] %>%
     ylab('Count')
 
 
-ggsave('het-cov.png', het.cov, width = 19.20, height = 10.80, units = 'in')
-ggsave('het-len.png', het.len, width = 19.20, height = 10.80, units = 'in')
-ggsave('het-sigdet.png', het.sigdet, width = 19.20, height = 10.80, units = 'in')
+## ggsave('het-cov.png', het.cov, width = 19.20, height = 10.80, units = 'in')
+## ggsave('het-len.png', het.len, width = 19.20, height = 10.80, units = 'in')
+## ggsave('het-sigdet.png', het.sigdet, width = 19.20, height = 10.80, units = 'in')
 
 ## standardized study
 std.wide <- read.csv('posterior-stdized.csv')
@@ -143,9 +143,9 @@ std.sigdet <- std[std$metric %in% c('tp','fp','tn','fn'),] %>%
     theme_bw(base_size = 30) +
     ylab('Count')
 
-ggsave('std-cov.png', std.cov, width = 19.20, height = 10.80, units = 'in')
-ggsave('std-len.png', std.len, width = 19.20, height = 10.80, units = 'in')
-ggsave('std-sigdet.png', std.sigdet, width = 19.20, height = 10.80, units = 'in')
+## ggsave('std-cov.png', std.cov, width = 19.20, height = 10.80, units = 'in')
+## ggsave('std-len.png', std.len, width = 19.20, height = 10.80, units = 'in')
+## ggsave('std-sigdet.png', std.sigdet, width = 19.20, height = 10.80, units = 'in')
 
 ## og study
 og.wide <- read.csv('posterior-og.csv')
@@ -180,9 +180,9 @@ og.sigdet <- og[og$metric %in% c('tp','fp','tn','fn'),] %>%
     ylab('Count')
 
 
-ggsave('og-cov.png', og.cov, width = 19.20, height = 10.80, units = 'in')
-ggsave('og-len.png', og.len, width = 19.20, height = 10.80, units = 'in')
-ggsave('og-sigdet.png', og.sigdet, width = 19.20, height = 10.80, units = 'in')
+## ggsave('og-cov.png', og.cov, width = 19.20, height = 10.80, units = 'in')
+## ggsave('og-len.png', og.len, width = 19.20, height = 10.80, units = 'in')
+## ggsave('og-sigdet.png', og.sigdet, width = 19.20, height = 10.80, units = 'in')
 
 ## compact plots
 
