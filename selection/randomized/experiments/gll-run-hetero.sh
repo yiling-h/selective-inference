f8bd21388a8462a39f669d76f19e42bced5f44aa
Dan Kessler <kesslerd@umich.edu>
2021-08-03 13:32:10 -0400

    throw more resources at hetero since it ran out of time previously

--- selection/randomized/experiments/gll-run-hetero.sh
+++ selection/randomized/experiments/gll-run-hetero.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 #SBATCH --job-name="posi-het"
-#SBATCH --ntasks=75
+#SBATCH --ntasks=305
 #SBATCH --mem-per-cpu=6gb
-#SBATCH --time=8:00:00
+#SBATCH --time=48:00:00
 #SBATCH --account=stats_dept1
 #SBATCH --partition=standard
 # The application(s) to execute along with its input arguments and options:
