799e2dbc727f16264088f3205d4cfd93982d9929
Dan Kessler <kesslerd@umich.edu>
2021-08-29 22:08:48 -0400

    give all jobs 3x time (up to 6 days to run)
    asking for 3x as many posi calls, so be conservative.
    
    hopefully will run much faster than that

--- selection/randomized/experiments/gll-run-og.sh
+++ selection/randomized/experiments/gll-run-og.sh
@@ -2,7 +2,7 @@
 #SBATCH --job-name="posi-og"
 #SBATCH --ntasks=305
 #SBATCH --mem-per-cpu=6gb
-#SBATCH --time=48:00:00
+#SBATCH --time=06-00:00:00
 #SBATCH --account=stats_dept1
 #SBATCH --partition=standard
 # The application(s) to execute along with its input arguments and options:
--- selection/randomized/experiments/gll-run-stdized.sh
+++ selection/randomized/experiments/gll-run-stdized.sh
@@ -2,7 +2,7 @@
 #SBATCH --job-name="posi-std"
 #SBATCH --ntasks=75
 #SBATCH --mem-per-cpu=6gb
-#SBATCH --time=8:00:00
+#SBATCH --time=06-00:00:00
 #SBATCH --account=stats_dept1
 #SBATCH --partition=standard
 # The application(s) to execute along with its input arguments and options:
